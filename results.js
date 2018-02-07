6cfc3ce7076c1d6d4f8d49560cf8353a53292f5b
PMP <34005746+periodically-makes-puns@users.noreply.github.com>
2018-02-07 10:36:09 -0500

    Update results.js

--- results.js
+++ results.js
@@ -1,95 +1,165 @@
-let weights = new Map();
-let weightedVotes = [];
-let resultProfiles = [];
-let avgs = [];
+function Profile(client, id, response, score, stdev, rank, prize, alive) {
+  this.user = client.users.get(id);
+  console.log(this.user);
+  this.name = this.user.username
+  this.response = response;
+  this.score = Math.floor(score * 10000)/100;
+  this.stdev = Math.floor(stdev * 10000)/100;
+  this.pay = Math.floor((score * 70 + stdev * 10)*10000)/100;
+  this.rank = rank;
+  this.prize = prize;
+  this.alive = alive;
+  this.ordinal = "th";
+  switch (rank % 10) {
+    case 1:
+      this.ordinal = "st";
+      break;
+    case 2:
+      this.ordinal = "nd";
+      break;
+    case 3:
+      this.ordinal = "rd";
+      break;
+  }
+  if ((rank % 100 < 14) && (rank%100 > 10)) {
+    this.ordinal = "th";
+  }
+  this.embed = {embed: 
+    {
+      color: 4369908,
+      author: {
+        name: this.user.username,
+        icon_url: this.user.avatarURL
+      },
+      title: this.name,
+      description: this.rank + this.ordinal + " place",
+      fields: [{name: "Response", value: this.response},{
+          name: "Score",
+          value: this.score + "%"
+        },
+        {
+          name: "Standard Deviation",
+          value: this.stdev + "%"
+        },
+        {
+          name: "Payment",
+          value: "$" + this.pay / 100
+        }
+      ],
+      timestamp: new Date(),
+      footer: {
+        iconURL: client.user.avatarURL,
+        text: (this.prize) ? "They'll receive a prize this round!" : ((this.alive) ? "They'll continue on to the next round." : "They're dead. RIP.")
+      }
+    }
+  }
+}
+let resp;
+let indice;
+let scores = [];
 let stdevs = [];
-let placements = [];
-let avg = (input_array) => {
-  return input_array.reduce(function (accumulator, currentValue) {return accumulator + currentValue})/input_array.length;
-}
-let stdev = (input_array) => {
-  let average = avg(input_array);
-  return Math.sqrt(input_array.reduce(function(accumulator, currentValue) {return accumulator + Math.pow(currentValue - average, 2)})/(input_array.length-1));
-}
-let lcm = (input_array) => {
-  let out = input_array;
-  if (toString.call(out) !== "[object Array]") { 
-    return  false;
+let second = [];
+function wsum(array, weights) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += (array[i] >= 0) ? array[i]*weights[i] : 0;
   }
-  let r1 = 0;
-  let r2 = 0;
-  var l = out.length;
-  for (let i = 0; i < l; i++) {
-    r1 = out[i] % out[i + 1];
-    if (r1 === 0) {
-      out[i+1] = (out[i] * out[i+1]) / out[i+1];
-    } else {
-      r2 = out[i + 1] % r1;
-      if (r2 === 0) {
-        out[i+1] = (out[i] * out[i+1]) / r1;
+  return sum;
+}
+function waverage(array, weights) {
+  let sum = wsum(array, weights);
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    count += (array[i] >= 0) ? weights[i] : 0;
+  }
+  return sum/count;
+}
+function wstdev(array, weights) {
+  let avg = waverage(array, weights);
+  let a = 0;
+  let count = 0;
+  for (let i = 0; i < array.length; i++ ){
+    a += (array[i] >= 0) ? ((Math.pow(Math.abs(avg-a),2)) * weights[i]) : 0;
+    count += (array[i] >= 0) ? weights[i] : 0;
+  }
+  return Math.sqrt(a/count);
+}
+let weights = [];
+let fs = require("graceful-fs");
+let config = require("./config.json");
+config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+let Interval = require("Interval");
+module.exports = (client, msg, ids, responses, votes) => {
+  // weight votes
+
+  for (let i = 0; i < votes.length; i++) {
+    indice = config.votecount.findIndex(function (currentValue) {return currentValue[0] == votes[i][1]});
+    resp = responses.findIndex(function(currentValue) {return currentValue[0] == votes[i][1]});
+    if (resp != -1) {
+      if (second.indexOf(votes[i][1]) == -1) {
+        second.push(votes[i][1]);
+        weights.push(1);
       } else {
-        out[i+1] = (out[i] * out[i+1]) / r2;
+        weights.push(1/config.votecount[indice][1]);
       }
-    }
-  }
-  return out[l - 1];
-}
-let voteArr = [];
-let thingsToSay = [];
-module.exports = (client, channelID, contestantList, responses, votes, prompt) => {
-  let l = contestantList.length;
-  // populate weights
-  // client: Client
-  // channelID: Snowflake
-  // contestantList: Array
-  // responses: Map
-  // votes: Map
-  for (let i = 0; i < votes.length; i++) {
-    if (weights.has(votes[i][0])) {
-      weights.set(votes[i][0], weights.get(votes[i][0]) + 1);
     } else {
-      weights.set(votes[i][0], 1);
+      console.log(indice);
+      weights.push(2/config.votecount[indice][1]);
     }
   }
-  let lcm = lcm(weights.valuelist);
-  for (let i = 0; i < votes.length; i++) {
-    for (let j = 0; j < lcm / weights.get(votes[i][0]); j++) {
-      weightedVotes.push(votes[i]);
+  // calculate weighted mean
+  let votescores = [];
+  for (let i = 0; i < responses.length; i++) {
+    votescores = [];
+    for (let j = 0; j < weights.length; j++) {
+      votescores.push(votes[j][i+2]);
     }
+    scores[i] = waverage(votescores, weights);
   }
-  for (let i = 0; i < l; i++) {
-    resultProfiles.push([])
-    voteArr = [];
-    for (let j = 0; j < weightedVotes.length; j++) {
-      if (weightedVotes[j][i+1] == -1) {
-        continue;
+  // calculate weighted stdev
+  for (let i = 0; i < responses.length; i++) {
+    votescores = [];
+    for (let j = 0; j < weights.length; j++) {
+      votescores.push(votes[j][i+2]);
+    }
+    stdevs[i] = wstdev(votescores, weights);
+  }
+  let ranks = [];
+  // find ranks
+  for (let i = 0; i < scores.length; i++) {
+    let count = 1;
+    for (let j = 0; j < scores.length; j++) {
+      if (scores[j] > scores[i]) {
+        count++;
+      } else if (scores[j] == scores[i] && stdevs[j] < stdevs[i]) {
+        count++;
       }
-      voteArr.push(weightedVotes[j][i+1]);
     }
-    avgs.push(avg(voteArr));
-    stdevs.push(stdev(voteArr));
+    ranks.push(count);
   }
-  for (let i = 0; i < l; i++) {
-    placements.push(1);
-    for (let j = 0; j < l; j++) {
-      if (avgs[i] < avgs[j]) {
-        placements[i]++;
-      } else if (avgs[i] == avgs[j] && stdevs[i] < stdevs[j]) {
-        placements[i]++;
+  // create ResultProfiles™
+  let profiles = [];
+  for (let i = 0; i < responses.length; i++) {
+    profiles.push(0);
+  }
+  for (let i = 0; i < responses.length; i++) {
+    let rank = ranks[i] - 1;
+    do {
+      if (profiles[rank] == 0) {
+        profiles[rank] = new Profile(client, ids[i], responses[i], scores[i], stdevs[i], ranks[i], false, true);
+        break;
+      } else {
+        rank++;
       }
-    }
-    resultProfiles.push([placements[i], responses[contestantList[i]], contestantList[i],avgs[i],stdevs[i]]);
+    } while (profiles[rank] != 0) 
   }
-  resultProfiles = resultProfiles.sort(function (a, b){return a[0] - b[0]});
-  client.channels.get(channelID).send("And now, for results!");
-  thingsToSay = ["Good luck to all you " + l + " competitors!", "And now, let's announce the winner for the prompt: " + prompt, "They won with a score of " + resultProfiles[0][4] + ", and an stdev of " + resultProfiles[0][5] + "."];
   let i = 0;
-  i = 0;
-  let firstPeople = resultProfiles.findIndex(function(element, index, array) {return element > 1});
-  if (firstPeople != 0) {
-    thingsToSay.push("But wait! " + firstPeople + "others also got the same exact score and stdev! Wow!");
-  }
-  thingsToSay.push("Let's see who this first placer(s) were!");
-  
-  
+  Interval.run(function() {
+    msg.channel.send(profiles[i].embed);
+    i++;
+  }, 5000).until(function(){
+    return i == profiles.length;
+  }).end(function(){
+    msg.channel.send("That's all!");
+  });
 }
